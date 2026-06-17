f4a7108c315e08174f15261b799d0c257ef331eb
Abhishek Rana <abhishekrana0743@gmail.com>
2026-06-17 23:35:37 +0530

    Flight agent, hotel agent, Itinerary agent and Final agent added

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from langchain_core.messages import (
 from langchain_groq import ChatGroq
 
 from tools.tavily_tool import tavily_search
-from tools.flight_tool import flight_search
+from tools.flight_tool import search_flights
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -24,3 +24,63 @@ llm = ChatGroq(
 )
 
 DATABSE_URL = os.getenv("DATABASE_URL")
+
+class TravelState(TypedDict):
+    messages: Annotated[list[AnyMessage], operator.add]
+    user_query: str
+    flight_results: str
+    hotel_results: str
+    itinerary: str
+    llm_calls: int
+
+# Flight Agent
+def flight_agent(state: TravelState):
+    query = state["user_query"]
+    flight_data = search_flights(query)
+    return {
+        "flight_results": flight_data,
+        "messages": [
+            AIMessage(content=f"Flight results fetched")
+        ],
+        "llm_calls": state.get("llm_calls", 0) + 1
+    }
+
+def hotel_agent(state: TravelState):
+    query = f"Best hotels for {state['user_query']}"
+    hotel_results = tavily_search(query)
+
+    return {
+        "hotel_results": hotel_results,
+        "messages": [
+            AIMessage(content="Hotel information fetched")
+        ],
+        "llm_calls": state.get("llm_calls", 0) + 1
+    }
+
+# Itinerary Agent
+def itinerary_agent(state: TravelState):
+
+    prompt = f"""
+    Create a travel itinerary.
+    User Query:
+    {state['user_query']}
+
+    Flight Results:
+    {state['flight_results']}
+
+    Hotel Results:
+    {state['hotel_results']}
+    """
+
+    response = llm.invoke([
+        SystemMessage(
+            content="You are an expert travel planner"
+        ),
+        HumanMessage(content=prompt)
+    ])
+
+    return {
+        "itinerary": response.content,
+        "messages": [response],
+        "llm_calls": state.get("llm_calls", 0) + 1
+    }
